eed42280ebd7385277a51a94605bba03aa7bbfe1
TrpimirPerkovic <trpimir.perkovic@gmail.com>
2024-02-22 01:35:48 +0100

    22.2.'24

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .form-card {
   background-color: whitesmoke;
   border-radius: 10px;
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,53 +1,99 @@
 <template>
-  <div class="about">
-    <h1>This is a signup page</h1>
+  <FormCard>
+    <div class="about">
+      <h1>Sign Up</h1>
 
-    <div class="row">
-      <div class="col"></div>
-      <div class="col">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              v-model="username"
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              v-model="password"
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword2">Repeat Password</label>
-            <input
-              v-model="passwordRepeat"
-              type="password"
-              class="form-control"
-              id="exampleInputPassword2"
-              placeholder="Password"
-            />
-          </div>
-          <button type="button" @click="signup" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
+      <div class="row">
+        <div class="col"></div>
+        <div class="col">
+          <form>
+            <div class="form-group" id="mail">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                v-model="username"
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1" id="pass">Password</label>
+              <input
+                v-model="password"
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword2" id="pass"
+                >Repeat Password</label
+              >
+              <input
+                v-model="passwordRepeat"
+                type="password"
+                class="form-control"
+                id="exampleInputPassword2"
+                placeholder="Password"
+              />
+            </div>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div class="col"></div>
       </div>
-      <div class="col"></div>
     </div>
-  </div>
+  </FormCard>
+  <SocialClub></SocialClub>
 </template>
 
-<script></script>
+<script>
+import SocialClub from "../components/SocialClub.vue";
+import FormCard from "../components/FormCard.vue";
+
+export default {
+  name: "SignUp",
+  components: {
+    FormCard,
+    SocialClub,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-family: monospace;
+}
+h1:hover {
+  filter: drop-shadow(0 0 0.2em #2e0423aa);
+  transition: all 0.5s ease-in-out;
+}
+.btn-primary {
+  margin-top: 15px;
+  background-color: black;
+  margin-left: 32.5%;
+  font-family: monospace;
+}
+.btn-primary:hover {
+  filter: drop-shadow(0 0 0.2em #2e0423aa);
+  transition: all 0.5s ease-in-out;
+}
+
+#mail {
+  margin-top: 10px;
+  font-family: monospace;
+}
+#pass {
+  margin-top: 10px;
+  font-family: monospace;
+}
+</style>
